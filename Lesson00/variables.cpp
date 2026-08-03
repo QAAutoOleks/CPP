eca138be2d3a3e801ced0de8ed158f998b6cf712
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2026-08-03 21:25:22 +0300

    new file:   Lesson00/variables modified:   Lesson00/variables.cpp

--- Lesson00/variables.cpp
+++ Lesson00/variables.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
-    int age = 40;
+    int altitude = 40;
     double speed = 34.5;
     bool engineRunning = true;
+
+    std::cout << "Altitude: ";
+    std::cout << altitude << "\n";
+
+    std::cout << "Speed: ";
+    std::cout << speed << "\n";
+
+    std::cout << "Enging Running: ";
+    std::cout << engineRunning << "\n";
+
+    return 0;
 }
