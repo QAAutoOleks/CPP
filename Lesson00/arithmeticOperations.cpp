f0030dd5b0548246f5b65e4ea448ab9fe6223fa1
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2026-08-06 20:48:27 +0300

    prqcticing

--- Lesson00/arithmeticOperations.cpp
+++ Lesson00/arithmeticOperations.cpp
@@ -4,6 +4,8 @@ int main()
 {
     int a = 2;
     double b = 2.5;
+    double c = a / b;
 
-    std::cout << "Multiplication = " << a * b;
+    std::cout << "Multiplication = " << a * b << "\n";
+    std::cout << "Dividing = " << c << "\n";
 }
