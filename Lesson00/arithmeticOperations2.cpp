32bfa1ba683265abcff59f9ca9bbd32f1cac7268
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2026-08-06 21:06:37 +0300

    practicing

---
+++ Lesson00/arithmeticOperations2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    double speed_mps = 150;
+    double time_seconds = 60;
+
+    double distance = speed * time;
+    double newSpeed = distance / 100;
+
+    std::cout << "Distance = " << distance << "\n";
+    std::cout << "New speed = " << newSpeed << "\n";
+}
