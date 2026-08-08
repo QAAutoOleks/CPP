ca2b8a5f5ef30cfb69bd45f055500ed5bbe37055
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2026-08-08 19:50:18 +0300

    practicing

--- Lesson00/typesVariables.cpp
+++ Lesson00/typesVariables.cpp
@@ -4,4 +4,6 @@ int main()
 {
 double a = 2.12345; // 15-16 decimal digits
 float b = 3.54321f; // 7 decimal digits of precision
+char c = 'A'; // single (!!!) quote
+std::string d = "Drone"; // double quotes
 }
