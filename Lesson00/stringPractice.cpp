a20517370a45d8f864a6a6f11fa0933117f01f9a
QAAutoOleks <oleksandr.tsupko@gmail.com>
2026-08-20 21:38:26 +0300

    stringPractice

---
+++ Lesson00/stringPractice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    std::string surname;
+
+    name = "John";
+    surname = "Doe";
+
+    std::cout << name + " " + surname << std::endl;
+}
